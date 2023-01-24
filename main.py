cc89a80f4ab5687ca3a14e4e1af3bd5432edfc63
Kamsi <ozorjikj@gmail.com>
2023-01-24 09:30:53 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("I'm Noel, a chatbot")
+print("Feel free to ask me some questions or hold a conversation with me")
+
+print("---------------------------------------------------------------------")
+print()
